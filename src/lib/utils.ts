2c72318306b766b4f08e9469021c31d613c2aa15
YunHai <yunhai@bybxbwg.fun>
2025-06-30 17:33:24 +0800

    feat(media): 新增多首音乐并优化音乐详情页面
    - 优化音乐详情页面背景颜色提取逻辑，提高色彩准确度
    - 更新 API 文档类型定义，增加 ReferenceObject 类型的 $ref 属性
    - 优化 API 文档页面布局，提升移动端用户体验
    - 移除未使用的 API 文档接口 APIDocument- 新增全局加载和博客加载组件，提升用户体验

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,5 @@
 import {type ClassValue, clsx} from "clsx"
 import {twMerge} from "tailwind-merge"
-import gsap from "gsap"
-import {AppRouterInstance} from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
